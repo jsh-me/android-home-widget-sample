56fdf2a9fe02c14e725bc346b508947117bf196f
jsh-me <ppm_it@naver.com>
2021-05-21 02:11:12 +0900

    Implement AppWidgetHelper

---
+++ app/src/main/java/com/jshme/covidwidget/presentation/ui/add/AppWidgetHelper.kt
@@ -0,0 +1,67 @@
+package com.jshme.covidwidget.presentation.ui.add
+
+import android.app.Activity
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import androidx.appcompat.app.AppCompatActivity
+import com.jshme.covidwidget.R
+
+class AppWidgetHelper(
+    private val context: Context
+) {
+    private var title: String? = null
+    private var description: String? = null
+    private var pendingIntent: (() -> PendingIntent)? = null
+    private var widgetId: Int = 0
+
+    private lateinit var appWidgetManager: AppWidgetManager
+
+    fun onInitialize(title: String? = null, description: String? = null): AppWidgetHelper {
+        appWidgetManager = AppWidgetManager.getInstance(context)
+        this.title = title
+        this.description = description
+        return this
+    }
+
+    fun setWidgetId(id: Int?): AppWidgetHelper {
+        this.widgetId = id ?: AppWidgetManager.INVALID_APPWIDGET_ID
+        return this
+    }
+
+    fun setTitleText(title: String?): AppWidgetHelper {
+        this.title = title
+        return this
+    }
+
+    fun setDescriptionText(description: String?): AppWidgetHelper {
+        this.description = description
+        return this
+    }
+
+    fun setOnClickListener(pendingIntent: () -> PendingIntent): AppWidgetHelper {
+        this.pendingIntent = pendingIntent
+        return this
+    }
+
+    fun build() {
+        RemoteViews(context.packageName, R.layout.layout_covid_appwidget).apply {
+            setTextViewText(R.id.title, title ?: "")
+            setTextViewText(R.id.description, description ?: "")
+            setOnClickPendingIntent(R.id.root, pendingIntent?.invoke())
+        }.also { views ->
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
+
+        val resultValue = Intent().apply {
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+        }
+
+        (context as AppCompatActivity).apply {
+            setResult(Activity.RESULT_OK, resultValue)
+            finish()
+        }
+    }
+}
